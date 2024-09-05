c2f43a13b76b2b887cf74e1514f64cc45baabc79
Foxhoundn <filip.witosz+textace@gmail.com>
2024-09-05 15:10:05 +0200

    Fix missing localization, tests now test for values

--- src/tests/QorusAppsCatalogue.test.ts
+++ src/tests/QorusAppsCatalogue.test.ts
@@ -8,32 +8,32 @@ describe('Qorus Apps Catalogue tests', () => {
     expect(QorusAppsCatalogue.apps).toHaveProperty('zendesk');
 
     forEach(QorusAppsCatalogue.apps, (app) => {
-      expect(app.display_name).toBeDefined();
-      expect(app.short_desc).toBeDefined();
-      expect(app.desc).toBeDefined();
+      expect(app.display_name).not.toBeFalsy();
+      expect(app.short_desc).not.toBeFalsy();
+      expect(app.desc).not.toBeFalsy();
 
       forEach(app.actions, (action) => {
-        expect(action.action).toBeDefined();
-        expect(action.app).toBeDefined();
-        expect(action.display_name).toBeDefined();
-        expect(action.short_desc).toBeDefined();
-        expect(action.desc).toBeDefined();
+        expect(action.action).not.toBeFalsy();
+        expect(action.app).not.toBeFalsy();
+        expect(action.display_name).not.toBeFalsy();
+        expect(action.short_desc).not.toBeFalsy();
+        expect(action.desc).not.toBeFalsy();
 
         if (action.options) {
           forEach(action.options, (option) => {
-            expect(option.display_name).toBeDefined();
-            expect(option.short_desc).toBeDefined();
-            expect(option.desc).toBeDefined();
-            expect(option.type).toBeDefined();
+            expect(option.display_name).not.toBeFalsy();
+            expect(option.short_desc).not.toBeFalsy();
+            expect(option.desc).not.toBeFalsy();
+            expect(option.type).not.toBeFalsy();
           });
         }
 
         if (action.response_type) {
           forEach(action.response_type, (responseType) => {
-            expect(responseType.display_name).toBeDefined();
-            expect(responseType.short_desc).toBeDefined();
-            expect(responseType.desc).toBeDefined();
-            expect(responseType.type).toBeDefined();
+            expect(responseType.display_name).not.toBeFalsy();
+            expect(responseType.short_desc).not.toBeFalsy();
+            expect(responseType.desc).not.toBeFalsy();
+            expect(responseType.type).not.toBeFalsy();
           });
         }
       });
