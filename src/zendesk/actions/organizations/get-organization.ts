fb8f77d5d72ba5f5873dae3e53b26278ccc79ea6
Foxhoundn <filip.witosz+textace@gmail.com>
2024-09-13 09:39:31 +0200

    chore: Update zendesk swagger API path

--- src/zendesk/actions/organizations/get-organization.ts
+++ src/zendesk/actions/organizations/get-organization.ts
@@ -1,5 +1,5 @@
 import { TQorePartialAction } from 'global/models/qore';
-import { ZENDESK_SWAGGER_API_PATH } from '../..';
+import { ZENDESK_SWAGGER_API_PATH } from '../../constants';
 export default {
   action: 'get_organization',
   swagger_path: `${ZENDESK_SWAGGER_API_PATH}organizations/{organization_id}/GET`,
